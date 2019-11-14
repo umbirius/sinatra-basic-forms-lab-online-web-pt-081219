adeede2a6d98be6c9d88860de0bd87d61384307b
Umberto Palazzo <upalazzo93@gmail.com>
2019-11-14 19:58:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
   
   post '/puppy' do 
     binding.pry
-    puppy = Puppy.new(params[])
+    puppy = Puppy.new(params[:name], params[:breed], params[:months_old])
     erb :display_puppy
   end 
 
