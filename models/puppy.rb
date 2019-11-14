bd1e90a08f6246ec9be9c04dc8b42d157b0f50b3
Umberto Palazzo <upalazzo93@gmail.com>
2019-11-14 19:56:38 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,7 +1,7 @@
 class Puppy 
   attr_accessor :name, :breed, :months_old
   
-  def initialize(name:, breed:, months_old:)
+  def initialize(name, breed, months_old)
     self.name = name 
     self.breed = breed 
     self.months_old = months_old
